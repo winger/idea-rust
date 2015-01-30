2d643d7adb0e619419ea242657a76ac8838e902f
Dan Yang <dsyang@fb.com>
2015-01-29 21:49:58 -0800

    #29 fix module syntax by removing ::

--- src/java/test/vektah/rust/ParserTest.java
+++ src/java/test/vektah/rust/ParserTest.java
@@ -32,6 +32,8 @@ public class ParserTest extends ParsingTestCase {
 	public void testiflet() { doTest(true); }
 	public void testarray() { doTest(true); }
 	public void testbounds() { doTest(true); }
+	public void testmodule() { doTest(true); }
+	public void testfailed() { doTest(true); }
 
 	@Override
 	protected String getTestDataPath() {
